52fefb8876f4ea1c488e61e0d963485214771639
felipe.dev <083felype@gmail.com>
2024-11-20 15:12:04 -0300

    feat: add products by categories screen

---
+++ src/app/(tabs)/categories/[id].tsx
@@ -0,0 +1,42 @@
+import { FlatList, StyleSheet, View } from 'react-native'
+import { ProductItem } from '../../../components/product-item'
+import { router, Stack, useLocalSearchParams } from 'expo-router'
+import { getCategoryById } from '../../../services/category'
+import { getProductsByCategory } from '../../../services/product'
+
+export default function Products() {
+  const { id } = useLocalSearchParams()
+  const idCategory = parseInt(id as string)
+
+  const category = getCategoryById(idCategory)
+
+  if (!category) {
+    return router.back()
+  }
+
+  const products = getProductsByCategory(idCategory)
+
+  return (
+    <View style={styles.container}>
+      <Stack.Screen options={{ title: category.title }} />
+      <FlatList
+        data={products}
+        renderItem={({ item }) => <ProductItem data={item} />}
+        keyExtractor={(item) => item.id.toString()}
+        style={styles.list}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  list: {
+    flex: 1,
+    width: '100%',
+    padding: 20,
+    marginBottom: 20,
+  },
+})
